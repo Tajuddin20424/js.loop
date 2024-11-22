3c65c50c1acf11491cda72fffc6261d590cddd69
Tajuddin20424 <mttajuddin01@gmail.com>
2024-11-22 21:58:37 +0600

    added the all js

---
+++ recap.js
@@ -0,0 +1,18 @@
+//const : value will not change
+//let : value can change
+//var : we should not use it
+
+//variable
+const amount = 200;
+
+//condition
+if(amount > 100){
+
+}
+
+else {
+
+}
+
+// array
+const number =[1, 2, 3, 4, 5];
